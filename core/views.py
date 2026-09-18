280d247509219aad8ed6c4a5355fa6bc86585e50
Doten Play Onyx <naelnays@gmail.com>
2026-09-18 11:27:52 +0200

    Clean up views.py by removing unused code
    Removed unused imports and the test_media function.

--- core/views.py
+++ core/views.py
@@ -5,11 +5,6 @@ from boutiques.models import Boutique
 
 from produits.models import Categorie
 
-from django.conf import settings
-from django.http import HttpResponse
-import os
-
-
 def home(request):
 
     produits = Produit.objects.all().order_by('-id')[:24]
@@ -23,10 +18,3 @@ def home(request):
     }
 
     return render(request, 'core/home.html', context)
-
-def test_media(request):
-    return HttpResponse(
-        f"MEDIA_ROOT = {settings.MEDIA_ROOT}<br>"
-        f"Existe = {os.path.exists(settings.MEDIA_ROOT)}"
-    )
-
