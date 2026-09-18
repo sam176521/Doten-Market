8e718f03c71993d41f356045206fd6380b3aa73f
Doten Play Onyx <naelnays@gmail.com>
2026-09-18 12:31:52 +0200

    Remove admin password reset function
    Removed the 'creer_admin' function that resets the admin password.

--- accounts/views.py
+++ accounts/views.py
@@ -74,19 +74,6 @@ def profil_vendeur(request, pk):
         'nombre_produits': nombre_produits,
     })
 
-from django.http import HttpResponse
-from django.contrib.auth import get_user_model
 
-def creer_admin(request):
-    User = get_user_model()
-
-    user = User.objects.filter(username="admin").first()
-
-    if user:
-        user.set_password("admin123456")
-        user.save()
-        return HttpResponse("Mot de passe réinitialisé")
-
-    return HttpResponse("Admin introuvable")
 
 
